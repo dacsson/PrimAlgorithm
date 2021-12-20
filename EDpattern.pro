7cfc60207a8c4bbabbbdcf0615b36342eb81a3d2
dacsson <sda20036@gmail.com>
2021-12-20 19:36:10 +0400

    added variants of uploading theory file

--- EDpattern.pro
+++ EDpattern.pro
@@ -34,4 +34,5 @@ else: unix:!android: target.path = /opt/$${TARGET}/bin
 
 DISTFILES += \
     Theory.html \
+    config.json \
     resources/index.html
--- theorywindow.cpp
+++ theorywindow.cpp
@@ -17,9 +17,16 @@ TheoryWindow::~TheoryWindow()
 
 void TheoryWindow::DisplayTheoryFile()
 {
-    // - Получить путь к файлу с материалом
+    // - Получить путь к файлу с материалом из файла с конфигурацией (путь к файлу возникает в файле с конфигурацией в момент инсталляции)
     QString filename = QFileDialog::getOpenFileName(this, "Открыть файл", QDir::homePath(), "HTML file (*.html)");
     QFile TheoryFile(filename);
+
+    // ! Другой вариант задать абсолютный путь (не рабочий вариант при создании инсталлятора)
+    // QFile TheoryFile("D:/University/Lectures/Material.html");
+
+    // ! Другой вариант просить пользователя загрузить файл собственноручно (допустимый вариант)
+    //  QString filename = QFileDialog::getOpenFileName(this, "Открыть файл", QDir::homePath(), "HTML file (*.html)");
+    //  QFile TheoryFile(filename);
 
     // - Если с файлом что-то случилось (например пользователь удалил), то вызвать окно с ошибкой
     if(!TheoryFile.open(QIODevice::ReadOnly))
